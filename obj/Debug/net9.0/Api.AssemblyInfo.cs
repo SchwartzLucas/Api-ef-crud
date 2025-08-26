11153ab652a9c8dcfed2b6607d638aab0e8c3551
SchwartzLucas <lucassdesouza10@gmail.com>
2025-08-26 19:55:31 -0300

    corrigido novamente .csproj

--- obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a70f8d4b78911cfb6a5710362b8cc245e950518")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9a3d2ecfdcd4f3c3b3e645781b5517a08df30b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
