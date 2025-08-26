26cbb1197ac8e3b9d9b2896860318f015e6bfea1
SchwartzLucas <lucassdesouza10@gmail.com>
2025-08-26 19:17:32 -0300

    Criado PUT para atualizar o contato

--- Controlles/ContatoController.cs
+++ Controlles/ContatoController.cs
@@ -28,5 +28,23 @@ namespace Api.Controlles
             if(contato == null) return NotFound();
             return Ok(contato);
         }
+
+        [HttpPut("Atualizar{id}")]
+        public IActionResult Atualizar(int id, Contato contato){
+
+            var contatoBanco = _context.Contatos.Find(id);
+
+            if(contatoBanco == null) return NotFound();
+
+            contatoBanco.Nome = contato.Nome;
+            contatoBanco.Telefone = contato.Telefone;
+            contatoBanco.Ativo = contato.Ativo;
+
+            _context.Contatos.Update(contatoBanco);
+            _context.SaveChanges();
+
+            return Ok(contatoBanco);
+            
+        }
     }
 }
